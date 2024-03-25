2640df93f8da4002085acfb61c1d5e8c507008fa
Delgado Trueba <jose.delgado@babel.es>
2024-03-25 23:16:55 +0100

    chore/mfe: add configuration

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,5 @@ app.use(i18n);
 app.mount('#app');
 
 let lang = window.sessionStorage.getItem('Language') || 'es';
+// @ts-ignore
 i18n.global.locale.value = lang;
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import federation from "@originjs/vite-plugin-federation";
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    federation({
+      name: "remoteHabitsMfe",
+      filename: "remoteHabitsMfe.js",
+      exposes: {
+        "./App": "./src/App.vue",
+      }
+    }),
   ],
   resolve: {
     alias: {
