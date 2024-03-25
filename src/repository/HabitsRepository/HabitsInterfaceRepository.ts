6b13ea15932355cfb3a16e42f4335ace200bd119
IsabelGR <grisabel97@outlook.com>
2024-03-25 15:59:03 +0100

    feat/A-1.6: service ant

--- src/repository/HabitsRepository/HabitsInterfaceRepository.ts
+++ src/repository/HabitsRepository/HabitsInterfaceRepository.ts
@@ -3,4 +3,5 @@ import type { HabitResponseModel } from "./model/response/HabitResponseModel";
 export interface HabitsInterfaceRepository {
   food(): Promise<HabitResponseModel>;
   restaurant(): Promise<HabitResponseModel>;
+  ant(): Promise<HabitResponseModel>;
 }
